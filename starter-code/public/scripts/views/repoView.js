73ee0a5957beceb908cf2dd65bd0758e2b717209
Darryl Davis <ddavis1306@gmail.com>
2017-08-31 16:06:30 -0700

    gituation

--- starter-code/public/scripts/views/repoView.js
+++ starter-code/public/scripts/views/repoView.js
@@ -10,7 +10,6 @@ var app = app || {};
     $about.find('ul').empty();
     $about.show().siblings().hide();
   };
-
   const render = Handlebars.compile($('#repo-template').text());
 
   // DONE: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
@@ -18,11 +17,11 @@ var app = app || {};
     //this function renders the repos to the about ul
     //it also get called in aboutController
     ui();
-
+//
     $('#about ul').append(
       app.repos.with('name').map(render)
     );
   };
-// 
+//
   module.repoView = repoView;
 })(app);
